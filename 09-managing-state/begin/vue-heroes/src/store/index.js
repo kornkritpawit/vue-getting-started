6089d79ed053369688e278a9a1d3920fac69ba6c
kornkritpawit <konsoo@hotmail.com>
2020-05-30 23:34:18 +0700

    finish begin unit 9

---
+++ 09-managing-state/begin/vue-heroes/src/store/index.js
@@ -0,0 +1,64 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import { dataService } from '../shared/data.service';
+import {
+  GET_HEROES,
+  ADD_HERO,
+  DELETE_HERO,
+  UPDATE_HERO
+} from './mutation-types';
+
+Vue.use(Vuex)
+
+const state = {
+  heroes: [
+    // { id:1, firstName: 'Holly', lastName: 'Smith', description: 'hello'}
+  ],
+};
+const mutations = {
+
+  // getHeroes(state, heroes) {
+  [GET_HEROES](state, heroes) {
+    state.heroes = heroes;
+  },
+  [ADD_HERO](state, hero) {
+    state.heroes.push(hero);
+  },
+  [DELETE_HERO](state, hero) {
+    state.heroes = [...state.heroes.filter(h => h.id !== hero.id)];
+  },
+  [UPDATE_HERO](state, hero) {
+    const index = state.heroes.findIndex(h => h.id === hero.id);
+    state.heroes.splice(index, 1, hero);
+    state.heroes = [...state.heroes];
+  },
+};
+const actions = {
+  async getHeroesAction({ commit }) {
+    const heroes = await dataService.getHeroes();
+    commit(GET_HEROES, heroes);
+  },
+  async addHeroAction({ commit }, hero) {
+    const addedHero = await dataService.addHero(hero);
+    commit(ADD_HERO, addedHero);
+  },
+  async deleteHeroAction({ commit }, hero) {
+    const deletedHeroId = await dataService.deleteHero(hero);
+    commit(DELETE_HERO, deletedHeroId);
+  },
+  async updateHeroAction({ commit }, hero) {
+    const updatedHero = await dataService.updateHero(hero);
+    commit(UPDATE_HERO, updatedHero);
+  }
+};
+const getters = {
+  getHeroById: state => id => state.heroes.find(h => h.id === id),
+};
+
+export default new Vuex.Store({
+  strict: process.env.NODE_ENV !== 'production',
+  state,
+  mutations,
+  actions,
+  getters,
+})
--- 09-managing-state/begin/vue-heroes/src/views/heroes.vue
+++ 09-managing-state/begin/vue-heroes/src/views/heroes.vue
@@ -61,13 +61,13 @@
 
 <script>
 import Modal from '@/components/modal';
-import { dataService } from '../shared';
-
+// import { dataService } from '../shared';
+import { mapState, mapActions } from 'vuex';
 export default {
   name: 'Heroes',
   data() {
     return {
-      heroes: [],
+      // heroes: [],
       heroToDelete: null,
       message: '',
       showModal: false,
@@ -80,6 +80,7 @@ export default {
     await this.loadHeroes();
   },
   methods: {
+    ...mapActions(['getHeroesAction', 'deleteHeroAction']),
     askToDelete(hero) {
       this.heroToDelete = hero;
       this.showModal = true;
@@ -90,18 +91,30 @@ export default {
     async deleteHero() {
       this.closeModal();
       if (this.heroToDelete) {
-        dataService.deleteHero(this.heroToDelete);
+        // dataService.deleteHero(this.heroToDelete);
+        // this.$store.dispatch('deleteHeroAction', this.heroToDelete);
+        await this.deleteHeroAction(this.heroToDelete);
       }
       await this.loadHeroes();
     },
     async loadHeroes() {
-      this.heroes = [];
+      // this.heroes = [];
       this.message = 'getting the heroes, please be patient';
-      this.heroes = await dataService.getHeroes();
+      // this.$store.state.heroes;
+      // this.heroes = await dataService.getHeroes();
+      // await this.$store.dispatch('getHeroesAction');
+      await this.getHeroesAction();
       this.message = '';
     },
   },
   computed: {
+    
+    // heroes() {
+    //   return this.$store.state.heroes;
+    // },
+    // ...mapState({heroes: state => state.heroes}),
+    // ...mapState({ heroes: 'heroes'}),
+    ...mapState(['heroes']),
     modalMessage() {
       const name =
         this.heroToDelete && this.heroToDelete.fullName
